632da0bf0d8870ce3624abeab3249e7dbc1782c0
Gancheekim <b07901133@ntu.edu.tw>
2022-12-27 13:58:55 +0800

    add arguments to proces video

--- options/base_options.py
+++ options/base_options.py
@@ -23,6 +23,8 @@ class BaseOptions():
     def initialize(self, parser):
         """Define the common options that are used in both training and test."""
         # basic parameters
+        parser.add_argument('--use_video', action='store_true')
+        parser.add_argument('--video_name', type=str)
         parser.add_argument('--dataroot', default='placeholder', help='path to images (should have subfolders trainA, trainB, valA, valB, etc)')
         parser.add_argument('--name', type=str, default='experiment_name', help='name of the experiment. It decides where to store samples and models')
         parser.add_argument('--easy_label', type=str, default='experiment_name', help='Interpretable name')
